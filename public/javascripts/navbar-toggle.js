b2e90abab9b7f898a2b3aa817f7657eb0b3d1875
seanmai <seandmai@gmail.com>
2018-05-04 23:44:06 -0600

    Completed animation and navbar styling

--- public/javascripts/navbar-toggle.js
+++ public/javascripts/navbar-toggle.js
@@ -3,9 +3,11 @@ $(window).scroll(function() {
 
     if(height  > 0) {
         $( ".navbar" ).removeClass( "navbar-custom navbar-dark bg-dark");
-        $( ".navbar" ).addClass( "navbar-light bg-faded");
+        $( ".navbar" ).addClass( "navbar-light bg-white");
     } else {
-        $( ".navbar" ).removeClass( "navbar-light bg-faded");
+        $( ".navbar" ).removeClass( "navbar-light bg-white");
         $( ".navbar" ).addClass( "navbar-custom navbar-dark bg-dark");
     }
 });
+
+// use bg-faded for transparent
